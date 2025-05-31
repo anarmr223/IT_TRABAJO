e85ef5761880b4f2ca7fda4c49bfa2a2861dfb92
anarmr223 <anaromegalla223@gmail.com>
2025-05-31 13:36:47 +0200

    BBDD subida

---
+++ TRABAJOIT_WEB/src/java/persistencia/tiem.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 31-05-2025 a las 13:35:14
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tiem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiendas`
+--
+
+CREATE TABLE `tiendas` (
+  `nombreTienda` varchar(255) NOT NULL,
+  `producto` varchar(255) NOT NULL,
+  `nombreVendedor` varchar(255) NOT NULL,
+  `ganancias` double NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncar tablas antes de insertar `tiendas`
+--
+
+TRUNCATE TABLE `tiendas`;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tiendas`
+--
+ALTER TABLE `tiendas`
+  ADD PRIMARY KEY (`nombreTienda`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
