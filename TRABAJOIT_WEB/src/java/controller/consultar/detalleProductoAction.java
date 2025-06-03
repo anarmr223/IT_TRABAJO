2054fc5d88c8a0350ba94b342292ead5d664ab2e
anarmr223 <anaromegalla223@gmail.com>
2025-06-03 19:33:09 +0200

    actualizacion

---
+++ TRABAJOIT_WEB/src/java/controller/consultar/detalleProductoAction.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.consultar;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author Asus
+ */
+public class detalleProductoAction extends ActionSupport {
+    
+    public detalleProductoAction() {
+    }
+    
+    public String execute() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
