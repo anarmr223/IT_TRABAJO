4070e5187515cb3dd8fa810fb172956ade3d746c
Usuario <Usuario@DESKTOP-DLHI8SL>
2025-06-08 21:05:40 +0200

    Alta suscripcion action

--- TRABAJOIT_WEB/src/java/controller/registrar/registroSuscripcionAction.java
+++ TRABAJOIT_WEB/src/java/controller/registrar/registroSuscripcionAction.java
@@ -5,7 +5,11 @@
  */
 package controller.registrar;
 
+import WS.VendedorWS;
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.util.logging.Logger;
+import java.util.List;
+import model.Vendedor;
 
 /**
  *
@@ -13,11 +17,51 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class registroSuscripcionAction extends ActionSupport {
     
+    private int idCuenta;
+    private String nombreTienda;
+    
+    
     public registroSuscripcionAction() {
     }
     
     public String execute() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        VendedorWS client = new VendedorWS();
+        
+        Vendedor vendedor = client.findByNombreTienda(nombreTienda).get(0);
+        
+        client.addSuscripcion(vendedor.getVendedorPK().getDni(),vendedor.getVendedorPK().getIdCuenta(), idCuenta);
+                
+        return SUCCESS;
+        
+        
     }
+
+    public int getIdCuenta() {
+        return idCuenta;
+    }
+
+    public void setIdCuenta(int idCuenta) {
+        this.idCuenta = idCuenta;
+    }
+
+    public String getNombreTienda() {
+        return nombreTienda;
+    }
+
+    public void setNombreTienda(String nombreTienda) {
+        this.nombreTienda = nombreTienda;
+    }
+
+    public static Logger getLOG() {
+        return LOG;
+    }
+
+    public static void setLOG(Logger LOG) {
+        ActionSupport.LOG = LOG;
+    }
+    
+    
+    
     
 }
