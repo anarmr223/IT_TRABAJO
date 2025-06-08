fe7de7768b0d0fc7a894e55dae1c465b386f602d
Usuario <Usuario@DESKTOP-DLHI8SL>
2025-06-08 12:07:13 +0200

    Añadidos actions para editar porducto

--- TRABAJOIT_WEB/src/java/WS/ProductoWS.java
+++ TRABAJOIT_WEB/src/java/WS/ProductoWS.java
@@ -10,6 +10,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+import model.Producto;
 
 /**
  * Jersey REST client generated for REST resource:we [model.producto]<br>
@@ -35,14 +36,16 @@ public class ProductoWS {
     }
 
     /**
-     * @param requestEntity request data@return response object (instance of responseType class)
+     * @param requestEntity request data@return response object (instance of
+     * responseType class)
      */
     public void create_XML(Object requestEntity) throws ClientErrorException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
 
     /**
-     * @param requestEntity request data@return response object (instance of responseType class)
+     * @param requestEntity request data@return response object (instance of
+     * responseType class)
      */
     public void create_JSON(Object requestEntity) throws ClientErrorException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
@@ -63,32 +66,40 @@ public class ProductoWS {
     public <T> T findAll_JSON(Class<T> responseType) throws ClientErrorException {
         return webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
-    
-    
-    public <T> T findProductosByTienda(GenericType<T> gn, String nombreTienda){
-        WebTarget resource=webTarget;
+
+    public <T> T findProductosByTienda(GenericType<T> gn, String nombreTienda) {
+        WebTarget resource = webTarget;
         return resource.path("producto").path(nombreTienda).request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(gn);
     }
-    
-    public void eliminarProductoPorId(int id){
-        
-       WebTarget resource= client.target(BASE_URI+"model.producto"+"/"+id);
-       
-       resource.request().delete();
-        
-        
-        
+
+    public void eliminarProductoPorId(int id) {
+
+        WebTarget resource = client.target(BASE_URI + "model.producto" + "/" + id);
+
+        resource.request().delete();
+
     }
-    
-    
+
     public <T> T findProductosByVendedor_XML(GenericType<T> gn, String dni, int idCuenta) throws ClientErrorException {
         WebTarget resource = webTarget.path("byVendedor").path(dni).path(String.valueOf(idCuenta));
         return resource.request(MediaType.APPLICATION_XML).get(gn);
     }
-    
+
+    public void edit_XML(Integer id, Producto requestEntity) throws ClientErrorException {
+        // Construye la URL incluyendo el ID del producto
+        WebTarget resource = webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
+        // Realiza la petición PUT con el objeto Producto en formato XML
+        resource.request(MediaType.APPLICATION_XML)
+                .put(javax.ws.rs.client.Entity.entity(requestEntity, MediaType.APPLICATION_XML));
+    }
+
+    public Producto find(Integer id) throws ClientErrorException {
+        WebTarget resource = webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
+        return resource.request(MediaType.APPLICATION_XML).get(Producto.class); // O MediaType.APPLICATION_JSON si prefieres
+    }
 
     public void close() {
         client.close();
     }
-    
+
 }
